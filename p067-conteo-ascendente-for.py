1c42bf04de5c9c5a77b6ff3108bc097d0c9dda51
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-07 13:42:00 -0600

    se agregaron programas p067 - p075

---
+++ p067-conteo-ascendente-for.py
@@ -0,0 +1,7 @@
+# p067-conteo-ascendente-for.py
+# Imprime números del 1 al 100 usando un ciclo for
+
+print(" Iniciando secuencia de conteo ascendente...")
+
+for i in range(1, 101, 1):
+    print(i, end=' ')
