326097cab431bb8d3b9c1309b4f756d3ab2664fb
fernandoapmex-cell <fernandoapmex@gmail.com>
2025-10-09 13:05:33 -0600

    Actualizacion

--- p081-suma-potencias.py
+++ p081-suma-potencias.py
@@ -6,12 +6,9 @@ x = float(input("Introduce el valor de x: "))
 n = int(input("Introduce el número de términos (n): "))
 suma_total = 0
 print(f"\nCalculando la serie S = x^1 + ... + x^{n}")
-# Bucle exterior para cada término de la serie
-for i in range(1, n + 1):
-    termino_actual = 1
-    # Bucle interior para calcular la potencia x^i
-    for j in range(i):
-        termino_actual *= x
-        print(f"Término {i}: {x}^{i} = {termino_actual}")
+termino_actual=1
+for j in range(n+1):
     suma_total += termino_actual
-print(f"\nEl resultado de la serie es: {suma_total}")
+    print(f'Termino {x}^{j} ={termino_actual}')
+    termino_actual *= x
+print('\n Suma de toda la serie:',suma_total)
